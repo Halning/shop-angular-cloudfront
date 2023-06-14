9c2ca631df70d8051f0b9729683536171f521fc6
andriikhomenko <icandrii.khomenko@americanwell.com>
2023-06-14 11:32:34 +0300

    Add agw

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,14 +6,14 @@ export const environment: Config = {
     product: 'https://69zaejeg8j.execute-api.us-east-1.amazonaws.com/prod',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://69zaejeg8j.execute-api.us-east-1.amazonaws.com/prod',
+    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
     import: false,
-    bff: true,
+    bff: false,
     cart: false,
   },
 };
